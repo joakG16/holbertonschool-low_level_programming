4c2fcad909fd9d6b496471e7c83ae7bc3741bfe8
joakG16 <4379@holbertonschool.com>
2022-06-06 06:37:24 -0700

    task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print a string using printf
+ * Return: zero
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+
+	return (0);
+}
+
