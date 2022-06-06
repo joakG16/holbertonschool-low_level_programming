b964ebdba3cb88c77b4967ff7543b3985730f1a5
joakG16 <4379@holbertonschool.com>
2022-06-06 06:18:18 -0700

    task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print a string using puts
+ * Return: zero
+ */
+int main(void)
+{
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+
+	return (0);
+}
