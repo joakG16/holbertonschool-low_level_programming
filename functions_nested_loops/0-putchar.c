eff96381954bc8aed03972849e88aea2447302d8
joakG16 <4379@holbertonschool.com>
2022-06-08 09:27:40 -0700

    0c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,13 +6,14 @@
  */
 int main(void)
 {
-	char str[] = "_putchar";
-	int i;
-
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar(str[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
