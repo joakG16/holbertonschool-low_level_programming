5cab1025e393919769b592dd65ba5658001887b1
joakG16 <4379@holbertonschool.com>
2022-06-08 12:41:49 -0700

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Function that prints the
+ *alphabet, in lowercase, followed by a new line. Ten times
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	j = 0;
+	while (j < 10)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		j++
+	}
+	_putchar('\n');
+}
