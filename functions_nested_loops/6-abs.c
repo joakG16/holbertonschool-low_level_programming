7eb4ea87a82714d4533556269b9862f8caf7c54b
joakG16 <4379@holbertonschool.com>
2022-06-09 05:26:35 -0700

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs -  a function that computes the absolute value of an integer.
+ *@n: number to be checked
+ *Return: 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+return (0);
+}
