0e0be52ce508ada72d66f93613a3d790967c33ab
joakG16 <4379@holbertonschool.com>
2022-06-09 13:20:30 -0700

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *add - a function that adds two integers and returns the result
+ *@a: number 1
+ *@b: number 2
+ *Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
