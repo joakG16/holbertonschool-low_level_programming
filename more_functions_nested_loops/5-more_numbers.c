07b8325e07c8fcc51310d9c8fe0a1aa3f30153ed
joakG16 <4379@holbertonschool.com>
2022-06-10 14:27:42 -0700

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *more_numbers - times the numbers, from 0 to 14, followed by a new line
+ */
+void more_numbers(void)
+{
+	int num;
+	int count;
+
+	count = 0;
+	while (count <= 10)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+		count++;
+	}
+}
