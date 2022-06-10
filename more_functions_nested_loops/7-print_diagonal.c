10edb818258f09822e50bc92e3577bc4db464fdc
joakG16 <4379@holbertonschool.com>
2022-06-10 16:54:17 -0700

    task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_diagonal - print a diagonal
+ *@n: number of diagonals
+ */
+void print_diagonal(int n)
+{
+	int count;
+	int spc;
+
+	if (n > 0)
+	{
+		for (count = 1; count <= n; count++)
+		{
+			for (spc = 1; spc <= n; spc++)
+			{
+				if (spc == count)
+				{
+					_putchar('\\');
+				}
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
