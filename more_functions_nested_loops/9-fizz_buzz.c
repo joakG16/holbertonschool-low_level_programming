c6eeb36b86c86f992500c9df030ff5b333f9f928
joakG16 <4379@holbertonschool.com>
2022-06-10 19:31:16 -0700

    task 10

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main - print numbers
+ *Return: 0 if succes
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+	return (0);
+}
