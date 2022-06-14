e73203a61b72d4b5344a16586fd7f450ce58dce9
joakG16 <4379@holbertonschool.com>
2022-06-14 09:28:01 -0700

    task 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *swap_int: swap values
+ *swap_int - swap values
  *@a: value a
  *@b: value b
  */
 void swap_int(int *a, int *b)
 {
-	int *c;
+	int c;
 
-	c = a;
+	c = *a;
 	*a = *b;
-	b = c;
+	*b = c;
 }
