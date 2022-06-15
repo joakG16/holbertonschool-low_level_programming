a25142f0936736657ec2c01064fc07b7632df9af
joakG16 <4379@holbertonschool.com>
2022-06-15 09:35:17 -0700

    task 6c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,34 @@
 #include "main.h"
 
 /**
+ *stlen - count num of char in passed string
+ *@s: string given
+ *Return: count
+ */
+int stlen(char *s)
+{
+	int count = 0;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+return (count);
+}
+
+/**
  *
  *
  */
 void puts2(char *str)
 {
 	int i;
-	
-	for (i = 0; str[i] != '\0'; i++)
+
+	for (i = 0; i < stlen(str); )
 	{
 		_putchar(str[i]);
-		i += 1;
+		i += 2;
 	}
 	_putchar('\n');
 }
