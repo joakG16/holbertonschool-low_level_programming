30d8a6fc30dc6d693bc8976b28fdcb27b82c1ebd
joakG16 <4379@holbertonschool.com>
2022-06-16 11:29:07 -0700

    task 3 betty

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
+ *_strcmp - string comparation
  *
- *
- *
+ *@s1: first string to compare
+ *@s2: second string
+ *Return: diference between s1 and s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -16,7 +18,7 @@ int _strcmp(char *s1, char *s2)
 			if (differ != 0)
 				break;
 	}
-	if(differ == 0)
-		return(0);
-return(differ);
+	if (differ == 0)
+		return (0);
+return (differ);
 }
