99acd5761c6fff2184575aff975cc4988be30113
joakG16 <4379@holbertonschool.com>
2022-06-17 05:51:14 -0700

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,6 +6,30 @@
  *@
  *Return: 
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+	char syb[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i, j;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= 32;
+			}
+		}
+		for (j = 0; syb[j]; j++)
+		{
+			if (str[i - 1] == syb[j])
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					str[i] -= 32;
+				}
+			}
+		}
+	}
+return (str);
 }
