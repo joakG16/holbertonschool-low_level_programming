5e937484122214eee18d8c95c6b07af5df493f4e
joakG16 <4379@holbertonschool.com>
2022-06-17 07:16:07 -0700

    task 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,11 +1,29 @@
 #include "main.h"
 
 /**
- *leet - 
+ *leet - encodes a string into 1337.
  *
- *@ 
- *Return:
+ *@str: string passed
+ *Return: string encoded
  */
-char *leet(char *)
+char *leet(char *str)
 {
+	char upp[] = {'a', 'e', 'o', 't', 'l'};
+	char low[] = {'A', 'E', 'O', 'T', 'L'};
+	char num[] = {'4', '3', '0', '7', '1'};
+	int i, j;
+
+	for (i = 0; str[i]; i++)
+	{
+		j = 0;
+		while (upp[j] && low[j] && num[j])
+		{
+			if (str[i] == upp[j] || str[i] == low[j])
+			{
+				str[i] = num[j];
+			}
+			j++;
+		}
+	}
+	return (str);
 }
