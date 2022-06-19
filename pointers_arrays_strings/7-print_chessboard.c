5222b7a0c026751976f82755ee22ee05bd83bd87
joakG16 <4379@holbertonschool.com>
2022-06-19 16:00:05 -0700

    task 6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			if (col == 7)
+			{
+				_putchar(a[row][col]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[row][col]);
+		}
+	}
+}
