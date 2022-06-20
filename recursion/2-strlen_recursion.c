bb4569a79a4e5b5e043f3e6a4e9fbc55f4882906
joakG16 <4379@holbertonschool.com>
2022-06-20 09:54:13 -0700

    task 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - 
+ *
+ *@s: string passed
+ *Return: string lenght
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen_recursion(s + 1) + 1);
+}
