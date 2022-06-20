1106ea466272defb26f8f97e6c7d88f1d36d09b4
joakG16 <4379@holbertonschool.com>
2022-06-20 12:37:51 -0700

    task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+int CheckPrime(int n,int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if(n % i == 0)
+		{
+			return(0);
+		}
+		else
+		{
+			return (CheckPrime(n, i - 1));
+		}
+	}
+}
+/**
+ *
+ */
+int is_prime_number(int n)
+{
+	return (CheckPrime(n, n / 2));
+}
