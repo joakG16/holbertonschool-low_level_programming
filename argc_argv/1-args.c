5eb02fe978ae95cc0607a53a99af285f22936c7e
joakG16 <4379@holbertonschool.com>
2022-06-21 08:20:56 -0700

    task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -17,7 +17,7 @@ int main(int argc, char __attribute__((unused)) **argv)
 		for (count = 0; count < argc; count++)
 		{
 		}
-		printf("%d\n", count);
+		printf("%d\n", count - 1);
 	}
 	
 	return (0);
