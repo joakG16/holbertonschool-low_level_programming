e414a444e691ccb8cda5b5c8e290131007c7a2a1
joakG16 <4379@holbertonschool.com>
2022-06-21 12:22:44 -0700

    task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int j = 0;
+	int sum = 0;
+
+	if (argc < 2)
+		printf("0\n");
+
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; j < (int)strlen(argv[i]); j++)
+			{
+				if (isdigit(argv[i][j]))
+				{
+					sum = sum + atoi(argv[i]);
+					break;
+				}
+				else
+				{
+					printf("Error\n");
+					return(1);
+				}
+			}
+		}
+		printf("%d\n", sum);
+	}
+
+return(0);
+}
