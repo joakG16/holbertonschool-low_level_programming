05d81d3153338b4d9cee5a14c2a02e002c4dab62
joakG16 <4379@holbertonschool.com>
2022-06-21 07:37:52 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - a program that prints the number of arguments passed into it.
+ *@argc: number of arguments passed
+ *@argv: pointer to the array of string containing the argument's name
+ *Return: 0 if success
+ */
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	int count = 0;
+
+	if (argc >= 0)
+	{
+		for (count = 0; count < argc; count++)
+		{
+		}
+		printf("%d\n", count);
+	}
+	
+	return (0);
+}
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,1 +1,2 @@
 int main(int __attribute__((unused)) argc, char **argv);
+int main(int argc, char __attribute__((unused)) **argv);
