6983ae56fbb2a1de8ec9df4d2cf17a32b4df8895
joakG16 <4379@holbertonschool.com>
2022-06-23 09:18:50 -0700

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ *
+ *
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	s = malloc(sizeof(char) * strlen(str) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	else
+	{
+		for (i = 0; i < (int)strlen(str); i++)
+		{
+			s[i] = str[i];
+		}
+		s[i] = '\0';
+	}
+	return(s);
+}
