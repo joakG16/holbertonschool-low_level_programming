faf0d63a4be071defc8e32d35c6101ce0912695a
joakG16 <4379@holbertonschool.com>
2022-06-23 06:57:07 -0700

    task 0, renember that h in hexa is 0x48

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,14 +10,20 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	unsigned int i;
 
 	if (size == 0)
 		return NULL;
 
-	str = malloc(sizeof(c) * size);
+	str = malloc(sizeof(char) * size);
 
 	if (str == NULL)
 		return NULL;
+	else
+	{
+		for (i = 0; i < size; i ++)
+			str[i] = c;
+	}
 
 	return(str);
 }
