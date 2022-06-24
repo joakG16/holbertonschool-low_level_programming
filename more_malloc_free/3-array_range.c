fdfdb8e248c8d368a04b1ba44686f65fa979cb3f
joakG16 <4379@holbertonschool.com>
2022-06-24 12:40:38 -0700

    task 3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *
+ *
+ *
+ */
+int *array_range(int min, int max)
+{
+	int *p;
+	int i, diff;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	diff = max - min;
+	p = malloc(sizeof(int) * diff + 1);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= diff; i++)
+	{
+		p[i] = min;
+		min++;
+	}
+	return(p);
+}
