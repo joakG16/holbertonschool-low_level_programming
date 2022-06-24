005cf26a71d9aa5c090ad3c995c32c2220b64d90
joakG16 <4379@holbertonschool.com>
2022-06-24 09:13:15 -0700

    task 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/*
+ *
+ *
+ *
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int lenS1 = strlen(s1);
+	unsigned int lenS2 = strlen(s2);
+	unsigned int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	p = malloc (sizeof(char) * lenS1 + lenS2 + 1);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < lenS1; i++)
+		{
+			p[i] = s1[i];
+		}
+		if (lenS2 < n)
+		{
+			for (j = 0; j < lenS2; j++)
+			{
+				p[i + j] = s2[j];
+			}
+			p[i + j] = '\0';
+		}
+		if (lenS2 > n)
+		{
+			for (j = 0; j < n; j++)
+			{
+				p[i + j] = s2[j];
+			}
+			p[i + j] = '\0';
+		}
+	}
+	return(p);
+}
