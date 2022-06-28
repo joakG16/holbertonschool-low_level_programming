7d12501aa385eff20cbea2ae8be0bee556e41041
joakG16 <4379@holbertonschool.com>
2022-06-28 15:44:21 -0700

    task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *
+ *
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dogg;
+
+	dogg = malloc(sizeof(dog_t));
+
+	if(dogg == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		dogg->name = malloc(sizeof(dogg->name) * (strlen(name) + 1));
+			if(dogg->name == NULL)
+			{
+				free(dogg);
+				return (NULL);
+			}
+			else
+				strcpy(dogg->name, name);
+
+		dogg->age = age;
+
+		dogg->owner = malloc(sizeof(dogg->owner) * (strlen(owner) + 1));
+			if(dogg->owner == NULL)
+			{
+				free(dogg);
+				return (NULL);
+			}
+			else
+				strcpy(dogg->owner, owner);
+	}
+	return(dogg);
+
+}
