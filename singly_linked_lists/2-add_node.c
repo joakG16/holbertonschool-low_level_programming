10a65913387d6550cbf5f7f55ef8fc11b76c0530
joakG16 <4379@holbertonschool.com>
2022-07-13 08:47:27 -0700

    task 2 betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
- *
- *
+ *add_node - a function that adds a new node at the beginning of a list_t list
+ *@head: point to the first node, which point to a structure of type list_t
+ *@str: value to fill the variable of the new node
+ *Return: the adress of the new element, NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -16,8 +18,11 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
-
-	return(*head);
+	new_node->next = *head; /* setting to where head is pointing to,
+				 * which is the first node
+				 */
+	*head = new_node; /* now head holds new_node, and
+			   * the ex-first is next to new_node
+			   */
+	return (*head);
 }
