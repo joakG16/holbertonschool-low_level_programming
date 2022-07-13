cc03ec9e28d1645f556d58987df3c93f38d77644
joakG16 <4379@holbertonschool.com>
2022-07-13 11:19:14 -0700

    task 4 betty

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,17 +1,21 @@
 #include "lists.h"
 
 /**
- *
- *
+ *free_list - a function that frees a list_t list.
+ *@head: pointer to node
  */
 void free_list(list_t *head)
 {
 	list_t *actualNode;
 
-	while(head != NULL)
+	while (head != NULL)
 	{
+		/* "save" the node it is currently standing
+		 * so when I free it, next doesn't get lost also
+		 */
 		actualNode = head;
 		head = head->next;
+		/* remember to free also the values of the node */
 		free(actualNode->str);
 		free(actualNode);
 	}
