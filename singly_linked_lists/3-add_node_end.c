fb68963dc5b23f3c2102216823126f43dee69cf8
joakG16 <4379@holbertonschool.com>
2022-07-13 10:41:35 -0700

    task 3 betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
- *
- *
+ *add_node_end - a function that adds a new node at the end of a list_t list.
+ *@head: point to the first node, which point to a structure of type list_t
+ *@str: value to fill the variable of the new node
+ *Return: the adress of the new element, NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -16,20 +18,27 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
+	/**
+	 *This new node is going to be the last node, so make next
+	 *of it as NULL
+	 */
 	new_node->next = NULL;
 
+	/* If the Linked List is empty, then make the new node as head */
 	if (*head == NULL)
 	{
 		*head = new_node;
 	}
 	else
 	{
+		/* traverse till the last node */
 		lastNode = *head;
-		while(lastNode->next != NULL)
+		while (lastNode->next != NULL)
 		{
 			lastNode = lastNode->next;
 		}
+		/* Change the next of last node */
 		lastNode->next = new_node;
 	}
-	return(new_node);
+	return (new_node);
 }
