bbc4552698cee5506f17d45f1539498324676640
joakG16 <4379@holbertonschool.com>
2022-07-14 16:07:12 -0700

    betty

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,11 @@
 #include "lists.h"
 
 /**
- *
- *
+ *get_nodeint_at_index - a function that returns the nth node
+ *of a listint_t linked list.
+ *@head: pointer to first node
+ *@index: desired node position, starting at 0
+ *Return: the nth node, NULL if the node doesn't exist
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
@@ -11,16 +14,16 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	currentnode = head;
 	i = 0;
 	while (currentnode != NULL)
 	{
 		if (i == index)
-			return(currentnode);
+			return (currentnode);
 		i++;
 		currentnode = currentnode->next;
 	}
-	return(NULL);
+	return (NULL);
 }
