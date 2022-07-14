cf193c230307753c8be8ebc3ea57bf225a39230a
joakG16 <4379@holbertonschool.com>
2022-07-14 06:49:42 -0700

    task 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *print_list - a function that prints all the elements of a listint_t list.
+ *@h: pointer to struct of type listint_t (pointing to the first node)
+ *Return: node counter
+ */
+size_t print_listint(const listint_t *h);
+{
+	int count = 0;
+
+	while (*h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
