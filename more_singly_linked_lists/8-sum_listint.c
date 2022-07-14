a0a4700ced53a15ed73431f97175c0ce007720f9
joakG16 <4379@holbertonschool.com>
2022-07-14 16:54:44 -0700

    betty

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
- *
- *
+ *sum_listint - a function that returns the sum of all the
+ *data (n) of a listint_t linked list.
+ *@head: pointer to first node
+ *Return: sum, 0 if empty
  */
 int sum_listint(listint_t *head)
 {
