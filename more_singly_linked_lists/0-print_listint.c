71087fa44e35773a37b09c1a510a068641d23fa3
joakG16 <4379@holbertonschool.com>
2022-07-14 06:56:13 -0700

    BETTY

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *print_list - a function that prints all the elements of a listint_t list.
+ *print_listint - a function that prints all the elements of a listint_t list.
  *@h: pointer to struct of type listint_t (pointing to the first node)
  *Return: node counter
  */
