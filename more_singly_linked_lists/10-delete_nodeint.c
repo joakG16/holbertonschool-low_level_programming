d3d94ad8e1474f0443fa775a4dc3d5152b1fe7d6
joakG16 <4379@holbertonschool.com>
2022-07-15 12:32:33 -0700

    combined

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -7,8 +7,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *current, *nextnode;
+	listint_t *previousindx, *current;
 
 	if (*head == NULL)
 	{
@@ -21,18 +20,18 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = current->next;
 		free(current);
-		return (1);
+		return(1);
 	}
-	for (i = 0; current != NULL && i < index - 1; i++)
+	while (--index && current != NULL)
+	{
+		previousindx = current;
 		current = current->next;
-
+	}
 	if (current == NULL || current->next == NULL)
 	{
 		return (-1);
 	}
-	nextnode = current->next->next;
-	free(current->next);
-	current->next = nextnode;
-
-	return(1);
+	previousindx->next = current->next;
+	free(current);
+	return (1);
 }
