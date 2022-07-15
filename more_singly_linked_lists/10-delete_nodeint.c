eed72f6cf0a046f2dadd11c32f72040870e2d803
joakG16 <4379@holbertonschool.com>
2022-07-15 10:47:22 -0700

    wrong condition

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -46,7 +46,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		previous = current;
 		current = current->next;
 	}
-	if (current == NULL || current->next == NULL)
+	if (current == NULL)
 	{
 		return (-1);
 	}
