052209712f5815e55d469c1070ea19dd01724a98
joakG16 <4379@holbertonschool.com>
2022-07-15 10:38:40 -0700

    task 10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+ *listint_len - a function that returns the number of elements
+ *in a linked listint_t list.
+ *@h: pointer to struct of type list_t
+ *Return: counter
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ *
+ *
+ *
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current, *previous;
+	unsigned int size;
+
+	size = listint_len(*head);
+
+	if (size < index || *head == NULL)
+	{
+		return(-1);
+	}
+	current = *head;
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return(-1);
+	}
+	while (--index)
+	{
+		previous = current;
+		current = current->next;
+	}
+	if (current == NULL || current->next == NULL)
+	{
+		return (-1);
+	}
+	previous->next = current->next;
+	free(current);
+
+	return (1);
+}
--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -37,12 +37,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode->next = NULL;
 
 	listsize = listint_len(*head);
-
+	/* if index given is bigger than my list */
 	if (idx > listsize)
 	{
 		return (NULL);
 	}
-
+	/* if i need to insert in the first node */
 	if (idx == 0)
 	{
 		newnode->next = *head;
@@ -51,11 +51,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	else
 	{
 		current = *head;
-
+		/* traverse until the index gets to 0 (inverted) */
 		while (--idx)
 			current = current->next;
-
+		/* assign new node's next to matched index's node's next */
 		newnode->next = current->next;
+		/* "move"(assign) matched index's node next to the new one */
 		current->next = newnode;
 	}
 	return (newnode);
