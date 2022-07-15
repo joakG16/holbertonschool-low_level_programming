d3771e050b652b71a9942d78983cef9e573f3f1b
joakG16 <4379@holbertonschool.com>
2022-07-15 12:35:41 -0700

    fixing border case

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		previousindx = current;
 		current = current->next;
 	}
-	if (current == NULL || current->next == NULL)
+	if (current == NULL)
 	{
 		return (-1);
 	}
