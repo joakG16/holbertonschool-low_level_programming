425c61a252eca4c95cc98c93673606260f5054fa
joakG16 <4379@holbertonschool.com>
2022-07-15 07:14:18 -0700

    task 9d, returning NULL at end error

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	newnode->n = n;
-	newnode->n = NULL;
+	newnode->next = NULL;
 
 	listsize = listint_len(*head);
 
