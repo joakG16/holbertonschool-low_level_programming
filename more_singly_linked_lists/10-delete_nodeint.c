e46ae054bf2b0a271b6fbdb8dba06454c8679d57
joakG16 <4379@holbertonschool.com>
2022-07-15 18:01:50 -0700

    betty

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ *delete_nodeint_at_index - a function that deletes the node at
+ *index index of a listint_t linked list.
+ *@head: double pointer to pointer to node
+ *@index: given index to remove node
+ *Return: 1 if succeed, -1 if fail
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
@@ -20,8 +22,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = current->next;
 		free(current);
-		return(1);
+		return (1);
 	}
+	/* if I decrement the value first, in a case of index = 1, */
+	/* the function wont work because index will be 0 when loop starts */
+	/* thats why it has to be decremented after being "first tested" */
 	while (index-- && current != NULL)
 	{
 		previousindx = current;
