679607c176baaf7bacda1954a6ca7b5983e60678
joakG16 <4379@holbertonschool.com>
2022-07-19 15:27:05 -0700

    task 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+	mask = 1 << index;
+
+	*n = *n & ~mask;
+
+	return (1);
+}
