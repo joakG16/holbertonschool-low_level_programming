762eda8f1057498262c56411b8a95c1fc2319754
joakG16 <4379@holbertonschool.com>
2022-07-19 11:38:28 -0700

    task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	char *binary;
+	unsigned int dec_val = 0;
+	int i = 0;
+	int base = 1;
+	int len = strlen(b);
+
+	binary = malloc(sizeof(char *) * len);
+	if (binary == NULL)
+	{
+		return (0);
+	}
+	binary = strcpy(binary, b);
+	for(i = len - 1; i >= 0; i--)
+	{
+		if(binary[i] != '0' && binary[i] != '1')
+		{
+			return (0);
+		}
+		if (binary[i] == '1')
+		{
+			dec_val = dec_val + base;
+		}
+		base = base * 2;
+	}
+	return(dec_val);
+}
