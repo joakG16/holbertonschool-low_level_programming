360b4735e958c8554c2411ac1d30a66449b8e876
joakG16 <4379@holbertonschool.com>
2022-07-20 10:16:28 -0700

    task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t rcount, wcount;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char *) * letters);
+	if (!buffer)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if(fd < 0)
+	{
+		free(buffer);
+		return(0);
+	}
+	rcount = read(fd, buffer, letters);
+	if (rcount < 0)
+	{
+		free(buffer);
+		return(0);
+	}
+	wcount = write(STDOUT_FILENO, buffer, rcount);
+	if (wcount != rcount || wcount < 0)
+	{
+		free(buffer);
+		return(0);
+	}
+	close(fd);
+	free(buffer);
+	return(wcount);
+}
