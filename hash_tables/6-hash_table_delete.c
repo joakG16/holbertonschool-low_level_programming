2e1b22dc33ce3a0e370002a49cfa13dc87a1a1a5
joakG16 <4379@holbertonschool.com>
2022-08-22 14:38:59 -0300

    task 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * @brief 
+ * 
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *item;
+	int i;
+
+	if (ht == NULL)
+		return;
+
+	for(i = 0; i < ht->size; i++)
+	{
+		item = ht->array[i];
+
+		if (item)
+		{
+			free(item->key);
+			free(item->value);
+			free(item);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
