8424ba063a7fbe233003d3d4a45b7f451360700a
joakG16 <4379@holbertonschool.com>
2022-08-22 14:45:05 -0300

    collision case

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -16,12 +16,13 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		item = ht->array[i];
 
-		if (item)
+		while (item) /* needs to check both only an item, and more (Lin.Lis) */
 		{
 			free(item->key);
 			free(item->value);
-			free(item);
+			item = item->next; /* in case of collision (multiple Lin.Lis) */
 		}
+		free(item);
 	}
 	free(ht->array);
 	free(ht);
