4e11e3bd2e5f5838653223e6fedf4f3220e17ac5
joakG16 <4379@holbertonschool.com>
2022-08-26 07:47:10 -0700

    task 1 border cases

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,12 +1,20 @@
 #include "search_algos.h"
 
 /**
+ *binary_search - a function that searches for a value in a sorted
+ *array of integers using the Binary search algorithm
+ *@array: a pointer to the first element of the array to search in 
+ *@size: the number of elements in array
+ *@value: the value to search for
  *
- *
+ *Return: the index where value is located, - 1 otherwise
  */
 int binary_search(int *array, size_t size, int value)
 {
 	size_t i = 0, left = 0, mid = 0, right = size - 1;
+
+	if (!array || !size || !value)
+		return (-1);
 
 	while (left <= right)
 	{
