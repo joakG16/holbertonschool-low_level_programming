0c4386034d7191aa8ebe431e2e3c819f671a66c7
joakG16 <4379@holbertonschool.com>
2022-08-26 06:53:55 -0700

    task 0 fixed, print and then ckeck and return index, not value

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,8 +1,13 @@
 #include "search_algos.h"
 
 /**
+ *linear_search - a function that searches for a value in an
+ *array of integers using the Linear search algorithm
+ *@array: a pointer to the first element of the array to search in
+ *@size: the number of elements in array
+ *@value: the value to search for
  *
- *
+ *Return: the first index where value is located, -1 if not found
  */
 int linear_search(int *array, size_t size, int value)
 {
@@ -13,9 +18,9 @@ int linear_search(int *array, size_t size, int value)
 
 	for (idx = 0; idx < size; idx++)
 	{
-		if (array[idx] == value)
-			return (array[idx]);
 		printf("Value checked array[%ld] = [%d]\n", idx, array[idx]);
+		if (array[idx] == value)
+			return (idx);
 	}
 	return (-1);
 }
